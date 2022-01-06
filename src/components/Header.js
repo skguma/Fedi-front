6f2fa3d4794862a3652e00a97b61fe907df9fa3e
ssini <cdnnnl@ewhain.net>
2022-01-06 22:51:44 +0900

    Design: Separate function and style of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,9 @@
 import React from "react";
 import "../style/style.css";
-import styled from "styled-components";
+import StyledHeader from "../style/StyledHeader";
+import styled, { css } from "styled-components";
 
-const HeaderWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 30px;
-  width: 100%;
-  border: 1px solid black;
-`;
+const { HeaderWrapper } = StyledHeader;
 
 const Header = () => {
   return <HeaderWrapper>FEDI</HeaderWrapper>;
---
+++ src/style/StyledHeader.js
@@ -0,0 +1,23 @@
+import styled, { css } from "styled-components";
+
+const HeaderWrapper = styled.div`
+  ${({ theme }) => {
+    const { fonts, colors } = theme;
+    return css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 50px;
+      font-size: ${fonts.size.base};
+      font-weight: ${fonts.weight.bold};
+      color: ${colors.blue};
+      background-color: ${colors.bgColor};
+      width: 100%;
+      border: 1px solid black;
+    `;
+  }}
+`;
+
+const StyledHeader = { HeaderWrapper };
+
+export default StyledHeader;
