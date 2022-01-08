67419b98f1e257c65c9149d52c93b20f0c7d7b79
ssini <cdnnnl@ewhain.net>
2022-01-08 23:43:10 +0900

    Refactor: Change descriptions' text into arrangement

--- src/pages/NetworkmapPage.js
+++ src/pages/NetworkmapPage.js
@@ -6,12 +6,24 @@ import Networkmap from "../components/Networkmap";
 import Footer from "../components/Footer";
 
 const NetworkmapPage = () => {
+  const information = [
+    { id: "0", note: "트위터 네트워크 맵" },
+    {
+      id: "1",
+      note: "신고할 계정들을 클릭한 다음, 하단의 신고하기 버튼을 눌러주세요"
+    },
+    {
+      id: "2",
+      note: "원본 트윗 보기 버튼을 누르면 해당 트윗의 url로 이동합니다"
+    }
+  ];
+  const nextPage = { destination: "report", name: "신고하기" };
   return (
     <div className="main-container">
       <Header />
-      <Description />
+      <Description information={information} />
       <Networkmap />
-      <PageMove />
+      <PageMove nextPage={nextPage} />
       <Footer />
     </div>
   );
