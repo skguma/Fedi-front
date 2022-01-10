66827ccca2b11f68480168114f871652aac55b3e
ssini <cdnnnl@ewhain.net>
2022-01-10 23:33:20 +0900

    Feat: Check email address

--- src/components/EmailBox.js
+++ src/components/EmailBox.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
+import "../style/style.css";
 
 const EmailBox = ({ changeState }) => {
+  const [isActive, setActive] = useState(false);
   const [values, setValues] = useState({ email: "" });
   const handleChange = e => {
     const { name, value } = e.target;
@@ -9,17 +11,35 @@ const EmailBox = ({ changeState }) => {
   };
   const handleSubmit = e => {
     e.preventDefault();
-    alert("이메일로 발송되었습니다.");
+    if (isActive) {
+      alert("이메일로 발송되었습니다.");
 
-    // TODO: 서버로 이메일 전송하기
-    // 부모의 centerReport를 false로 만들기
-    changeState();
+      // TODO: 서버로 이메일 전송하기
+      // 부모의 centerReport를 false로 만들기
+      changeState();
+    } else {
+      alert("이메일에 @를 포함해서 입력해주세요.");
+    }
+  };
+
+  const checkValid = () => {
+    values.email.includes("@") ? setActive(true) : setActive(false);
   };
   return (
     <Wrapper>
       <form onSubmit={handleSubmit}>
-        <input value={values.email} name="email" onChange={handleChange} />
-        <button type="submit">전송</button>
+        <input
+          value={values.email}
+          name="email"
+          onKeyUp={checkValid}
+          onChange={handleChange}
+        />
+        <button
+          className={isActive ? "activebtn" : "unactivebtn"}
+          type="submit"
+        >
+          전송
+        </button>
       </form>
     </Wrapper>
   );
