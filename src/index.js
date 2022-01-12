202b9cd60b398833f80f3bf8722e7624b0ce328c
ssini <cdnnnl@ewhain.net>
2022-01-13 01:06:16 +0900

    Feat: Add redux-persist

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
+import logger from "redux-logger";
+import ReduxThunk from "redux-thunk";
 
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+); // 미들웨어 적용
+const persistor = persistStore(store);
+
+console.log(store.getState()); // 스토어의 상태 확인
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
