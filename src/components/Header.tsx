9d57d470ee4c6dd947762426606809f76a100df6
hyosin-Jang <cdnnnl@gmail.com>
2022-03-21 20:11:53 +0900

    Refactor: Fixed header to top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import { theme } from '../style/theme';
+import { theme, flexCenter } from '../style/theme';
 
 const Header = () => {
   return <HeaderWrapper>FEDI</HeaderWrapper>;
 };
 
 const HeaderWrapper = styled.div`
+  ${flexCenter}
+  width: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
   height: 50px;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 100;
   font-size: 14px;
   font-weight: bold;
   color: ${theme.color.blue};
-  background-color: ${theme.color.bgColor};
-  width: 100%;
+  background-color: ${theme.color.white};
 `;
 
 export default Header;
