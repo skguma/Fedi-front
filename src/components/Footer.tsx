ea6aa7654f88369d52c13f96fffea9aed65ca2a3
hyosin-Jang <cdnnnl@gmail.com>
2022-03-22 14:13:42 +0900

    Feat: Add center shortcut

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,63 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-import { theme } from '../style/theme';
-
-const FooterWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 200px;
-  width: 100%;
-  background-color: ${theme.color.bgColor};
-`;
+import styled from 'styled-components';
+import { theme, flexCenter } from '../style/theme';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
-  return <FooterWrapper></FooterWrapper>;
+  const { t } = useTranslation(['page']);
+
+  return (
+    <FooterWrapper>
+      <Button onClick={() => window.open(`https://d4u.stop.or.kr/`, '_blank')}>
+        <Message>{t('page:Footer.center')} </Message>
+        {t('page:Footer.shortcut')}
+      </Button>
+      <Contact>
+        Contact <br />
+        feditection@gmail.com
+      </Contact>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
+
+const FooterWrapper = styled.div`
+  ${flexCenter}
+  bottom: 0;
+  flex-direction: column;
+  height: 200px;
+  width: 100%;
+  background-color: ${theme.color.blue};
+`;
+
+const Contact = styled.div`
+  align-self: flex-start;
+  color: ${theme.color.white};
+  width: 90%;
+  font-size: 13px;
+  line-height: 20px;
+  padding-bottom: 20px;
+  padding-top: 20px;
+  border-top: 1px solid white;
+  margin: 20px;
+`;
+
+const Button = styled.button`
+  border: 1px solid white;
+  border-radius: 50px;
+  width: 280px;
+  cursor: pointer;
+  background-color: white;
+  color: black;
+  font-weight: bold;
+  padding: 18px;
+  margin-top: 40px;
+  margin-bottom: 10px;
+  font-size: 18px;
+`;
+
+const Message = styled.div`
+  font-size: 12px;
+  color: grey;
+`;
