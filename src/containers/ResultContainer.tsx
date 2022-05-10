4eaeebf7954a7f1519fc768e03eb12bb7b7ed8a1
hyosin-Jang <cdnnnl@gmail.com>
2022-05-10 13:15:32 +0900

    Add empty result page

--- src/containers/ResultContainer.tsx
+++ src/containers/ResultContainer.tsx
@@ -4,10 +4,18 @@ import ResultBoard from '../components/ResultBoard';
 import { select, unselect } from '../modules/reports';
 import { suspends } from '../modules/suspends';
 import { getResults } from '../modules/results';
+import PageMove from '../components/PageMove';
 import SkeletonUi from '../components/Skeleton';
+import EmptyResult from '../components/EmptyResult';
+import { useTranslation } from 'react-i18next';
+
 function ResultContainer() {
   const dispatch = useDispatch();
-
+  const { t } = useTranslation(['page']);
+  const nextPage: {
+    destination: string;
+    name: string;
+  } = { destination: 'report', name: t('page:NetworkmapPage.nextPage') };
   const { file } = useSelector((state) => ({
     file: state.images.file,
   }));
@@ -27,15 +35,17 @@ function ResultContainer() {
 
   if (loading) return <SkeletonUi />;
   if (error) return <div>에러 발생!</div>;
-  if (!data) return null;
-
-  return (
+  if (!data || data.length ===0) return <EmptyResult/>;
+  else return (
+    <>
     <ResultBoard
       results={data}
       onSelect={onSelect}
       onSuspend={onSuspend}
       onUnselect={onUnselect}
     />
+    <PageMove nextPage={nextPage} />
+    </>
   );
 }
 
